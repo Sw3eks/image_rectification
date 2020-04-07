cf35c275fe974d951a9e95fdea9e551f9d4e7804
Fabian Saacke <fabiansaacke@googlemail.com>
2020-04-07 08:05:03 +0200

    ppm and Rectification start

--- src/Rectification.java
+++ src/Rectification.java
@@ -1,3 +1,4 @@
+import org.opencv.calib3d.Calib3d;
 import org.opencv.core.Mat;
 import org.opencv.imgcodecs.Imgcodecs;
 
@@ -17,8 +18,18 @@ public class Rectification {
         images.add(image_2);
     }
 
-    public void doRectification() {
+    public void doRectification(Mat Po1, Mat Po2) {
+        Mat A1 = new Mat();
+        Mat A2 = new Mat();
+        Mat R1 = new Mat();
+        Mat R2 = new Mat();
+        Mat t1_new = new Mat();
+        Mat t2_new = new Mat();
+        Calib3d.decomposeProjectionMatrix(Po1, A1, R1, t1_new);
+        Calib3d.decomposeProjectionMatrix(Po2, A2, R2, t2_new);
 
+        System.out.println("A1: " + A1.dump());
+        System.out.println("A2: " + A2.dump());
     }
 
 }
